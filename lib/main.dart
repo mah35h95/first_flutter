8a3219b1be004272dcd7fd0178b0c7f33d90e5f5
Mahesh G <maheshgshno3@gmail.com>
2022-05-15 15:30:45 +0530

    ID card

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,93 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Home(),
+      home: NinjaCard(),
     ));
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-          centerTitle: true,
-          backgroundColor: Colors.red[600],
-        ),
-        body: Row(
-          
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text('Ninja ID card'),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Expanded(
-              flex: 3,
-              child: Image.asset('assets/imageSpace.jpg')
-              ),
-            Expanded(
-              flex: 1,
-              child: Container(
-               padding: EdgeInsets.all(30),
-               color: Colors.cyan,
-               child: Text('1'),
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/imageSpace.jpg'),
+                radius: 40,
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Container(
-               padding: EdgeInsets.all(30),
-               color: Colors.pinkAccent,
-               child: Text('2'),
-              ),
+            Divider(
+              height: 60,
+              color: Colors.grey[800],
             ),
-            Expanded(
-              flex: 1,
-              child: Container(
-               padding: EdgeInsets.all(30),
-               color: Colors.amber,
-               child: Text('3'),
-              ),
+            Text(
+              'NAME',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
             ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Mahesh G',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(color: Colors.grey, letterSpacing: 2),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '8',
+              style: TextStyle(
+                  color: Colors.amberAccent[200],
+                  letterSpacing: 2,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10,),
+                Text(
+                  'mahesh.g@mymail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1,
+                  ),
+                )
+              ],
+            )
           ],
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () => {},
-          child: Text('Click'),
-          backgroundColor: Colors.red[600],
-        ));
+      ),
+    );
   }
 }
